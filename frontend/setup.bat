5b31af2acc4a2a72f9149645671ca6e07e2f739a
Harishma0701 <harishma@rooman.net>
2025-10-15 05:38:54 +0000

    redundancy

--- frontend/setup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Installing dependencies...
-call npm install -D tailwindcss postcss autoprefixer
-call npm install @monaco-editor/react xterm xterm-addon-fit framer-motion react-icons @types/styled-components @types/xterm styled-components
-
-echo "\nSetup complete! Run 'npm run dev' to start the development server."
